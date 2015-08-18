31163020ad5a472edb8fa293fc56cfe35ad736c4
Sahid S Almas <robinaf3@gmail.com>
2015-08-18 21:41:11 +0530

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,10 +1,16 @@
-BASE=../../../vendor/manufacturer/devicename/proprietary
-rm -rf $BASE/*
+#!/bin/sh
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-mkdir -p $BASE/$DIR
-  fi
-adb pull /system/$FILE $BASE/$FILE
+VENDOR=samsung
+DEVICE=fortuna
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+echo "Pulling device files..."
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
 done
+on
