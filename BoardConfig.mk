4471128cd8060f278e85142d7214ea84e303b336
Sergey Karbainov <karbainovs@mail.ru>
2015-08-18 17:53:28 +0400

    CPU

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,9 @@
 # BoardConfig.mk
 # Under construction
 #
+TARGET_CPU_ABI 				:= armeabi-v7a
+TARGET_CPU_ABI2 			:= armeabi
+TARGET_ARCH 				:= arm
+TARGET_ARCH_VARIANT 			:= armv7-a-neon
+TARGET_ARCH_VARIANT_CPU 		:= cortex-a53
+TARGET_BOARD_PLATFORM 			:= msm8916
