037f34a418714fe67e5f5bc3068e987f4a3bf93d
AndroidFire <robinaf3@gmail.com>
2015-08-18 19:01:40 +0530

    added source

---
+++ extract-files.sh
@@ -0,0 +1,10 @@
+BASE=../../../vendor/manufacturer/devicename/proprietary
+rm -rf $BASE/*
+
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+mkdir -p $BASE/$DIR
+  fi
+adb pull /system/$FILE $BASE/$FILE
+done
