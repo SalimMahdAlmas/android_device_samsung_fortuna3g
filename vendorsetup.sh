7488824e93c60851e9341eff5ca0b690b21abceb
Sahid S Almas <robinaf3@gmail.com>
2015-08-20 15:32:10 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,6 +19,7 @@
 #
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
+#
+# 
 
-add_lunch_combo fortuna3g-eng
 add_lunch_combo fortuna3g-userdebug
