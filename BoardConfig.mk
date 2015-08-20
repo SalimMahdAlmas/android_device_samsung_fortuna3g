0f6b6aa82c2c68e1fd024f77c72f4f4607c33186
winkarbik <karbainovs@mail.ru>
2015-08-20 19:58:35 +0900

    fix build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,17 +21,17 @@ LOCAL_PATH :=  $(call my-dir)
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
 # CPU
-TARGET_ARCH_VARIANT_CPU 		:= cortex-a53
+TARGET_CPU_VARIANT 		:= cortex-a53
 
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_KERNEL_SOURCE := kernel/samsung/fortuna
+BOARD_CUSTOM_BOOTIMG_MK 	:= $(LOCAL_PATH)/mkbootimg.mk
+BOARD_KERNEL_BASE 		:= 0x80000000
+BOARD_KERNEL_CMDLINE 		:= console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1
+BOARD_KERNEL_PAGESIZE 		:= 2048
+BOARD_KERNEL_SEPARATED_DT 	:= true
+BOARD_RAMDISK_OFFSET 		:= 0x01000000
+TARGET_KERNEL_SOURCE 		:= kernel/samsung/fortuna
 # TARGET_KERNEL_CONFIG := fortuna3g_defconfig
 
 
@@ -41,16 +41,16 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Audio
 AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_ALSA_AUDIO 		:= true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/fstab.qcom
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_RECOVERY_DENSITY := hdpi
+TARGET_RECOVERY_FSTAB 		:= $(LOCAL_PATH)/recovery/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 	:= true
+BOARD_HAS_LARGE_FILESYSTEM	:= true
+TARGET_RECOVERY_DENSITY 	:= hdpi
 
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGER_ENABLE_SUSPEND 	:= true
+BOARD_CHARGER_SHOW_PERCENTAGE 	:= true
