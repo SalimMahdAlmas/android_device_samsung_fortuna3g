2368945865ec1dc014204825cdaae229aeb85a2e
Sergey Karbainov <karbainovs@mail.ru>
2015-09-10 20:12:19 +0800

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,66 +15,67 @@
 
 # BoardConfig.mk
 # We are working on this file so it may eat your cat
-LOCAL_PATH := device/samsung/fortuna3g
+LOCAL_PATH 					:= device/samsung/fortuna3g
 
 
 # Inherit from samsung qcom-common
 -include device/samsung/qcom-common/BoardConfigCommon.mk 
 
 # Platform
-TARGET_BOARD_PLATFORM := MSM8216
-TARGET_BOARD_PLATFORM_GPU := Adreno-306
+TARGET_BOARD_PLATFORM 				:= MSM8216
+TARGET_BOARD_PLATFORM_GPU 			:= Adreno-306
 
 # Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_GLOBAL_CFLAGS 				+= -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS 				+= -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS 				+= -DNO_SECURE_DISCARD
 
 # Architecture
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_ARCH_VARIANT_CPU := cortex-a53
-TARGET_CPU_ABI_LIST := arm64-v8a,armeabi-v7a,armeabi
-TARGET_CPU_ABI_LIST_32_BIT := armeabi-v7a,armeabi
-TARGET_CPU_ABI_LIST_64_BIT := arm64-v8a
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_VFP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT 				:= cortex-a53
+TARGET_ARCH_VARIANT_CPU 			:= cortex-a53
+TARGET_CPU_ABI_LIS				:= arm64-v8a,armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_32_BIT 			:= armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_64_BIT 			:= arm64-v8a
+ARCH_ARM_HAVE_NEO				:= true
+ARCH_ARM_HAVE_VFP 				:= true
+ARCH_ARM_HAVE_TLS_REGISTER 			:= true
 
 WITH_DEXPREOPT := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK 	:= $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_BASE 		:= 0x80000000
-BOARD_KERNEL_CMDLINE 		:= console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1
-BOARD_KERNEL_PAGESIZE 		:= 2048
-BOARD_KERNEL_SEPARATED_DT 	:= true
-BOARD_RAMDISK_OFFSET 		:= 0x01000000
-TARGET_KERNEL_SOURCE 		:= kernel/samsung/fortuna
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_CUSTOM_BOOTIMG_MK 			:= $(LOCAL_PATH)/mkbootimg.mk
+BOARD_KERNEL_BASE 				:= 0x80000000
+BOARD_KERNEL_CMDLINE 				:= console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1
+BOARD_KERNEL_PAGESIZE 				:= 2048
+BOARD_KERNEL_SEPARATED_DT 			:= true
+BOARD_KERNEL_TAGS_OFFSET 			:= 0x01E00000
+BOARD_RAMDISK_OFFSET     			:= 0x02000000
+TARGET_KERNEL_SOURCE 				:= kernel/samsung/fortuna
+BOARD_BOOTIMAGE_PARTITION_SIZE 			:= 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE 		:= 16777216
+BOARD_FLASH_BLOCK_SIZE 				:= 131072
 # We have'nt add add this because we are manging kernel source
 # TARGET_KERNEL_CONFIG := fortuna3g_defconfig
 
 
 # SD
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_SUPPRESS_EMMC_WIPE 	:= true
+TARGET_USE_CUSTOM_LUN_FILE_PATH 		:= /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR 		:= true
+BOARD_SUPPRESS_EMMC_WIPE 			:= true
 
 # Audio
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-BOARD_USES_ALSA_AUDIO 		:= true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY 		:= true
+BOARD_USES_ALSA_AUDIO 				:= true
 
 # Recovery
-TARGET_RECOVERY_FSTAB 		:= $(LOCAL_PATH)/recovery/fstab.qcom
-TARGET_USERIMAGES_USE_EXT4 	:= true
-BOARD_HAS_LARGE_FILESYSTEM	:= true
-TARGET_RECOVERY_DENSITY 	:= hdpi
-BOARD_HAS_LARGE_FILESYSTEM 	:= true
-TARGET_USERIMAGES_USE_EXT4 	:= true
+TARGET_RECOVERY_FSTAB 				:= $(LOCAL_PATH)/recovery/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 			:= true
+BOARD_HAS_LARGE_FILESYSTEM			:= true
+TARGET_RECOVERY_DENSITY 			:= hdpi
+BOARD_HAS_LARGE_FILESYSTEM 			:= true
+TARGET_USERIMAGES_USE_EXT4 			:= true
 
 # Charger
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND 	:= true
-BOARD_CHARGER_SHOW_PERCENTAGE 	:= true
+BOARD_CHARGER_DISABLE_INIT_BLANK 		:= true
+BOARD_CHARGER_ENABLE_SUSPEND 			:= true
+BOARD_CHARGER_SHOW_PERCENTAGE 			:= true
