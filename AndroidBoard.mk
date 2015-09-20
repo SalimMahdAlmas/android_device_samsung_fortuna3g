641fa15897c0e8c961a233b851ede0596fb49649
Sahid S Almas <robinaf3@gmail.com>
2015-09-20 09:32:37 +0530

    added vendor makefile

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -18,8 +18,8 @@ LOCAL_PATH :=  $(call my-dir)
 # Boot files
 #
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-
 file := $(INSTALLED_KERNEL_TARGET)
 ALL_PREBUILT += $(file)
 $(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
     $(transform-prebuilt-to-target)
+-include vendor/samsung/fortuna3g/AndroidBoardVendor.mk
