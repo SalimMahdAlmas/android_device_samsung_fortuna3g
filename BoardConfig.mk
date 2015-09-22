2a518a6f43b4202071257e4f512e12f45a57d2d7
Sahid S Almas <robinaf3@gmail.com>
2015-09-22 22:14:00 +0530

    Try setting selinux to permissive
    now it should be permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ TARGET_QCOM_MEDIA_VARIANT 			:= caf-new
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK 			:= $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE 				:= 0x80000000
-BOARD_KERNEL_CMDLINE 				:= console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE 				:= console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE 				:= 2048
 BOARD_KERNEL_SEPARATED_DT 			:= true
 BOARD_KERNEL_TAGS_OFFSET 			:= 0x01E00000
