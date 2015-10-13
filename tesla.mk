9a6fd8e4de1f576b26adea763774ded686c3f236
Noel Macwan <yulespencer@gmail.com>
2015-10-13 21:42:32 +0530

    tesla: bringup

--- tesla.mk
+++ tesla.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common TeslaOS stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_fortuna3g.mk)
@@ -12,7 +12,3 @@ PRODUCT_DEVICE := fortuna3g
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SM-G530H
-
-# Extra overlay for HDPI
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/hdpi
-
